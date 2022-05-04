7e79d0080d63fe6a32979fe83fc13696230996bc
Carlos Pavajeau <cantte098@gmail.com>
2022-05-04 09:34:21 -0500

    feat(products): add auth guard

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { RegisterProductComponent } from './components/register-product/register-product.component'
+import { AuthGuard } from '@auth0/auth0-angular'
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterProductComponent,
+    canActivate: [AuthGuard]
   }
 ]
 
